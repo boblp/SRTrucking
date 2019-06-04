06e821c3e8589b9f68a1bd544d406260f139ceae
boblp <robertolozano0011@gmail.com>
2019-06-04 18:43:00 -0500

    billing get

---
+++ routes/billing-get/_billing-get.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const config = require('../../util/config.js');
+const collectionName = config.collections.vendors;
+
+module.exports.handler = function(request, h){
+	const promise = new Promise((resolve, reject) => {
+		try{
+			jwt.verify(request.query.auth, 'secret', function(err, decoded) {
+		    	if (err){
+		    		resolve('Invalid Code or Level'); 
+		    	} else {
+		    		main(request, function(response){
+						resolve(response);
+					});
+		    	}
+		    });
+		}catch(e){
+			resolve(e.message);
+		}
+	}); 
+
+	return promise;
+}
+
+const main = function(request, callback){
+	const collection = request.mongo.db.collection(collectionName);
+
+	const query = {
+		origin: { $ne: '' },
+		destiny: { $ne: '' }
+	};
+
+	const deckQuery = {
+		POD: { $eq: '' },
+        rc: { $eq: '' },
+        invoice: { $eq: ''}
+	};
+
+	if (request.query.origin){
+		query.origin = request.query.origin
+	}
+
+	if (request.query.destination){
+		query.origin = request.query.destination
+	}
+
+	if (request.query.isDelivered){
+		deckQuery.POD = request.query.isDelivered
+	}
+
+	if (request.query.hasRC){
+		deckQuery.rc = request.query.hasRC
+	}
+
+	if (request.query.hasClientInvoice){
+		deckQuery.invoiceClient = request.query.hasClientInvoice
+	}
+
+	var pipeline = [{
+	    $unwind: '$decks'
+	},{
+	    $match: query
+	},{
+	    $replaceRoot: { 
+	        newRoot: "$decks" 
+	    } 
+	},{
+	    $match: deckQuery
+	}];
+
+	collection.aggregate(pipeline, {}, function(err, result) {
+		if(err){ callback(err); }else{
+			callback(result);
+		}
+	});
+}
---
+++ routes/billing-get/billing-get.js
@@ -0,0 +1,27 @@
+const joi = require('joi'); 
+var handler = require('./_'+__filename.split(/[\\/]/).pop()); 
+
+module.exports = { 
+	method: 'get',
+	path: '/billing-get',
+	config: { 
+		description: 'Get billing data - returns billing data',
+		notes: 'Get billing data - returns billing data',
+		tags: ['api'],
+		validate: {
+			query: {
+				auth: joi.string().required(),
+				origin: joi.string(),
+				destination: joi.string(),
+				isDelivered: joi.boolean().default(false).description('View delivered orders, by POD'),
+				hasRC: joi.boolean().default(false).description('Searches by RC'),
+				hasClientInvoice: joi.boolean().default(false).description('Searches by clientInvoice')
+			}, 
+			failAction: async (request, h, err) => {
+		        return err;
+			}
+		}
+	}, handler: async (request, h) => { 
+		return handler.handler(request, h); 
+	} 
+}
