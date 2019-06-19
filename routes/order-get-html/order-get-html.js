70933e174b1c71dd790592cd6eaff19205a5e690
Luis Hilario <luishilariomtz@gmail.com>
2019-06-19 03:21:30 -0500

    Se agrega ruta para obtener el html desde back para master y operaciones

---
+++ routes/order-get-html/order-get-html.js
@@ -0,0 +1,31 @@
+const joi = require('joi'); 
+var handler = require('./_'+__filename.split(/[\\/]/).pop()); 
+
+module.exports = { 
+	method: 'get',
+	path: '/order-get-html',
+	config: { 
+		description: 'Get order - returns order data',
+		notes: 'Get order - returns order data',
+		tags: ['api'],
+		validate: {
+			query: {
+				auth: joi.string().required(),
+				createdAt: joi.string(),
+				type: joi.string(),
+				location: joi.string(),
+				mineOnly: joi.boolean(),
+				id: joi.string(),
+				get_type: joi.string(),
+				viewComplete: joi.boolean().default(false).description('View complete orders'),
+				delivered: joi.boolean().description('View delivered orders'),
+				daysBack: joi.number().description('Days to look back')
+			}, 
+			failAction: async (request, h, err) => {
+		        return err;
+			}
+		}
+	}, handler: async (request, h) => { 
+		return handler.handler(request, h); 
+	} 
+}
